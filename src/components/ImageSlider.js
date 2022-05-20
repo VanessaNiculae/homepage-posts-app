2e1d9869a9147bab7e28622c96d9364c41c741ab
Vanessa Niculae <vanessa.alexandra.niculae@gmail.com>
2022-05-20 17:33:55 +0300

    slider-part1

---
+++ src/components/ImageSlider.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { SliderData } from './SliderData'
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
+
+const ImageSlider = ({ slides }) => {
+    const [current, setCurrent] = useState(0)
+    const length = slides.length
+
+    if (!Array.isArray(slides) || slides.length <= 0) {
+        return null;
+    }
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1)
+    };
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1)
+
+    };
+
+    return (
+        <section className="slider">
+            <FaArrowAltCircleLeft classname="left-arrow" onClick={prevSlide} />
+            <FaArrowAltCircleRight classname="right-arrow" onClick={nextSlide} />
+            {SliderData.map((slide, index) => {
+                return (
+                    <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                        {index === current && (< img src={slide.image} alt="me" className='image' />)}
+
+                    </div>
+                )
+
+            })}
+        </section>
+    );
+};
+
+export default ImageSlider;
---
+++ src/components/SliderData.js
@@ -0,0 +1,27 @@
+import ImageOne from './images/image.jpg';
+import ImageTwo from './images/image1.jpg';
+import ImageThree from './images/image2.jpg';
+import ImageFour from './images/image3.jpg';
+import ImageFive from './images/image4.jpg';
+
+export const SliderData = [
+    {
+        image: ImageOne
+    },
+
+    {
+        image: ImageTwo
+    },
+
+    {
+        image: ImageThree
+    },
+
+    {
+        image: ImageFour
+    },
+
+    {
+        image: ImageFive
+    }
+]
