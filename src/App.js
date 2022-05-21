d3c5ed42be6e33b6e32550a9aed2ccb7db3e99be
Vanessa Niculae <vanessa.alexandra.niculae@gmail.com>
2022-05-21 10:10:54 +0300

    button-text-included

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import './Posts.js';
 import ImageSlider from './components/ImageSlider';
 import { SliderData } from './components/SliderData'
 
 
 function App() {
-  return <ImageSlider slides={SliderData} />
+  return (
+    <>
+      <ImageSlider slides={SliderData} />
+
+      <div>
+        <header className='introduction'>
+
+          Hello my name is Vanessa. <br />
+          I love my dogs (I'm an animal lover), making people laugh, <br />
+          travelling, playing guitar, creating memories and photograph them.
+
+        </header>
+      </div>
+
+      <div>
+        <a href='./Posts.js' target='_blank'>
+          <button className='button'>
+            See my posts.
+          </button>
+        </a>
+      </div>
+    </>
+  );
+
 }
 
 export default App;
