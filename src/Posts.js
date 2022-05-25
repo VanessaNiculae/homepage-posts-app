392b66ab7acb339f15a5c24ed93fb28199ba69af
Vanessa Niculae <vanessa.alexandra.niculae@gmail.com>
2022-05-25 10:15:44 +0300

    Routing_Posts

--- src/Posts.js
+++ src/Posts.js
@@ -1,10 +1,17 @@
 
 import React from 'react'
 import GithubRepo from './components/GithubRepo';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function Posts() {
     return (
+        // <BrowserRouter>
+        //     <Switch>
+        //         <Route path="/">
         <GithubRepo />
+        //          </Route>
+        //     </Switch>
+        // </BrowserRouter> 
     );
 }
 
