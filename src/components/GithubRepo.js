eb512e2f9970ce92192dd042c7cefc08780e22ef
Vanessa Niculae <vanessa.alexandra.niculae@gmail.com>
2022-05-26 11:53:44 +0300

    Github_repo_rewriting

--- src/components/GithubRepo.js
+++ src/components/GithubRepo.js
@@ -1,32 +1,33 @@
-import jquery from 'jquery';
-import React from 'react'
-import $ from 'jquery';
+import React, { useEffect } from 'react';
 
+const getRepositories = setRepositories => fetch('https://api.github.com/users/VanessaNiculae/repos')
+    .then(response => response.json())
+    .then(setRepositories)
 
 const GithubRepo = () => {
-    var request = new XMLHttpRequest();
-    request.open('GET', 'https://api.github.com/users/VanessaNiculae/repos',
-        true)
-    request.onload = function () {
-        var data = JSON.parse(this.response);
-        console.log(data);
-        var statusHTML = '';
-        $.each(data, function (i, status) {
-            statusHTML += '<div class="card"> \
-                <a href=""> \
-                    <h4>' + status.name + '</h4> \
-                    <div class="state"> \
-                        <span class="mr-4"><i class="fa fa-star mr-2"></i>' + status.stargazers_count + '</span> \
-                        <span class="mr-4"><i class="fa fa-code-fork mr-2"></i>' + status.forks_count + '</span> \
-                    </div> \
-                </a> \
-            </div>';
-        });
-        $('.repositories').html(statusHTML);
-    }
-    request.send();
+    const [repositories, setRepositories] = React.useState();
+
+    useEffect(() => {
+        console.log('rep', repositories)
+        getRepositories(setRepositories)
+    })
+
+    return repositories.map(repository => <div key={repository.id} className="card" style={{ cursor: 'pointer' }} onClick={() => {
+        window.location.href = repository.html_url
+    }}>
+        <h4>{repository.name}</h4>
+        <div className="state">
+            <span classname="mr-4">
+                <i className="fa fa-star mr-2" />
+                {repository.stargazers_count}
+            </span>
+            <span className="mr-4">
+                <i className="fa fa-code-fork mr-2" />
+                {repository.forks_count}
+            </span>
+        </div>
+    </div>
+    )
 }
 
-
-
-export default GithubRepo;
+export default GithubRepo
